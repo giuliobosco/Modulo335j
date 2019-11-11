65f128bc3c62d7bea3aa64ce2c804425f3dfd7c5
giuliobosco <giuliobva@gmail.com>
2019-11-11 21:58:40 +0100

    fix MainActivity

--- app/src/main/java/ch/samt/bosco/modulo335j/MainActivity.java
+++ app/src/main/java/ch/samt/bosco/modulo335j/MainActivity.java
@@ -1,40 +1,42 @@
 package ch.samt.bosco.modulo335j;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import ch.samt.bosco.modulo335j.ui.ToastFragment;
+import ch.samt.bosco.modulo335j.ui.basetools.BaseToolsFragment;
+import ch.samt.bosco.modulo335j.ui.baseviewselements.BaseViewsElementsFragment;
+import ch.samt.bosco.modulo335j.ui.data.DataFragment;
+import ch.samt.bosco.modulo335j.ui.layouts.LayoutFragment;
+import ch.samt.bosco.modulo335j.ui.userinterfaces.UserInterfacesFragment;
 
 import android.view.Menu;
-import android.view.View;
-import android.widget.Toast;
 
-/*
-bundle.putString("url", "link");
-fragment = new WebViewFragment();
-fragment.setArgument(bundle);
- */
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
+
+    private AppSettings settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,30 +44,39 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/giuliobosco/Modulo335j"));
-                startActivity(browserIntent);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+        navigationView.setNavigationItemSelectedListener(this);
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_toast)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setCheckedItem(R.id.nav_home);
+        replace(new HomeFragment(settings));
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -83,27 +94,60 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.nav_host_fragment, new SettingsFragment(settings)).addToBackStack(null);
+            ft.commit();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
         int id = item.getItemId();
-        Toast.makeText(this, "PORCODIO fuori", Toast.LENGTH_LONG).show();
+        NavigationView navigationView = findViewById(R.id.nav_view);
 
         switch (id) {
-            case R.id.nav_toast:
-                Toast.makeText(this, "PORCODIO", Toast.LENGTH_LONG).show();
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                ft.replace(R.id.flMain, new ToastFragment());
-                ft.commit();
+            case R.id.nav_home:
+                replace(new ToastFragment());
+                navigationView.setCheckedItem(R.id.nav_view);
+                break;
+            case R.id.baseToolsDashIcon:
+                replace(new BaseToolsFragment(settings));
+                navigationView.setCheckedItem(R.id.nav_base_tools);
+                break;
+            case R.id.baseViewsElementsDashIcon:
+                replace(new BaseViewsElementsFragment(settings));
+                navigationView.setCheckedItem(R.id.nav_base_views_elements);
+                break;
+            case R.id.layoutsDashIcon:
+                replace(new LayoutFragment(settings));
+                navigationView.setCheckedItem(R.id.nav_layouts);
+                break;
+            case R.id.userInterfacesDashIcon:
+                replace(new UserInterfacesFragment(settings));
+                navigationView.setCheckedItem(R.id.nav_user_interfaces);
+                break;
+            case R.id.dataDashIcon:
+                replace(new DataFragment(settings));
+                navigationView.setCheckedItem(R.id.nav_data);
                 break;
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-
         return true;
+    }
+
+    private void replace(Fragment f) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.nav_host_fragment, f);
+        ft.commit();
     }
 }
